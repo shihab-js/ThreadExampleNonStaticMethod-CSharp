81f7d17e3141ce2dd362a51c6cfc40eefc88ffc0
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-29 23:57:15 +0600

    Program done...

--- ThreadExampleNonStaticMethod/Program.cs
+++ ThreadExampleNonStaticMethod/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Threading;
 
+
+public class MyThared
+{
+    public void Thread1()
+    {
+        for(int i=0; i<10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
 namespace ThreadExampleNonStaticMethod
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            MyThared mt = new MyThared();
+
+            Thread t1 = new Thread(new ThreadStart(mt.Thread1));
+            Thread t2 = new Thread(new ThreadStart(mt.Thread1));
+
+            t1.Start();
+            t2.Start();
         }
     }
 }
